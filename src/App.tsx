22fd34deb524ff5626d37454948887004edba941
Synxndh <117395956+Synxndh@users.noreply.github.com>
2023-02-22 01:43:49 +0700

    c

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ const App = () => {
                     }}
                 >
                     <RootNavigation />
+                    hjhhkjhhkh
                 </SafeAreaProvider>
             </PersistGate>
         </Provider>
