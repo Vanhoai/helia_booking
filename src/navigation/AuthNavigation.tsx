365010c886ee91a4a43b1fca12fe59b88c5799a9
Synxndh <117395956+Synxndh@users.noreply.github.com>
2023-02-22 15:38:25 +0700

    feat: marker_navigation

--- src/navigation/AuthNavigation.tsx
+++ src/navigation/AuthNavigation.tsx
@@ -1,19 +1,22 @@
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import {Login, Register} from '../screens/Auth';
-import {routes} from './routes';
+import { Login, Register, Splash, Marker } from '../screens/Auth';
+import { routes } from './routes';
 
 const AuthStack = createNativeStackNavigator();
 
 export const AuthNavigation = () => {
-  return (
-    <AuthStack.Navigator
-      initialRouteName={routes.login}
-      screenOptions={{
-        headerShown: false,
-      }}>
-      <AuthStack.Screen name={routes.login} component={Login} />
-      <AuthStack.Screen name={routes.register} component={Register} />
-    </AuthStack.Navigator>
-  );
+    return (
+        <AuthStack.Navigator
+            initialRouteName={routes.marker}
+            screenOptions={{
+                headerShown: false,
+            }}
+        >
+            <AuthStack.Screen name={routes.splash} component={Splash} />
+            <AuthStack.Screen name={routes.marker} component={Marker} />
+            <AuthStack.Screen name={routes.login} component={Login} />
+            <AuthStack.Screen name={routes.register} component={Register} />
+        </AuthStack.Navigator>
+    );
 };
